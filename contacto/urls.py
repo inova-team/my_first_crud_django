24540cf72d534c8d25b762691be916827d068590
__aduii__ <alexjuep@hotmail.com>
2023-07-01 05:32:31 -0500

    CRUD Contact Finished

--- contacto/urls.py
+++ contacto/urls.py
@@ -5,4 +5,7 @@ from contacto import views
 urlpatterns = [
     path('contacto/', views.contacto, name='contact'),
     path('editar_informacion_personal/', views.editar_informacion_personal, name='edit_pi'),
+    path('registro/', views.registrarse, name='signup'),
+    path('iniciar_sesion/', views.iniciar_sesion, name='login'),
+
     ]
--- contacto/views.py
+++ contacto/views.py
@@ -1,69 +1,88 @@
 from datetime import datetime
-
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from crud.models import InformacionPersonal
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth import authenticate, login
+
 
 @login_required
 def contacto(request):
     user = request.user
+    user_pi = InformacionPersonal.objects.get(usuario=user)
+    edad = datetime.now().year - user_pi.fecha_nacimiento.year
     context = {
-    "user": user
+        "user": user,
+        "user_pi": user_pi,
+        "edad":edad
     }
     return render(request, 'contacto/contacto.html', context)
 
+
+def iniciar_sesion(request):
+    if request.method == 'POST':
+        username = request.POST['input_username']
+        password = request.POST['input_password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('contact')
+
+    return render(request, 'contacto/iniciar_sesion.html')
+
+
+def registrarse(request):
+    if request.method == 'POST':
+        email = request.POST['input_email']
+        first_name = request.POST['input_first_name']
+        last_name = request.POST['input_last_name']
+        username = request.POST['input_username']
+        password = request.POST['input_password']
+        birthday = request.POST['input_birthday']
+        profesion = request.POST['input_profesion']
+        pais = request.POST['input_country']
+
+        if User.objects.filter(username=username).exists():
+            return redirect('signup')
+
+        user_create = User(email=email, username=username, password=make_password(password), first_name=first_name,
+                           last_name=last_name)
+        user_create.save()
+        user = User.objects.get(username=username)
+        InformacionPersonal.objects.create(fecha_nacimiento=birthday, edad=None, profesion=profesion, pais=pais,
+                                           usuario=user)
+        return redirect('login')
+
+
+    return render(request, 'contacto/registro.html')
+
+
 @login_required
 def editar_informacion_personal(request):
-    usuario = request.user
-    print(":::")
-    print(usuario)
-    informacion_personal, creado = InformacionPersonal.objects.get_or_create(usuario=usuario)
-    #TODO:: Debo implementar las vistas de Signup
-    #Alex Juep
-
-    #devuelve false si hay informacion personal de ese usuario
-    #creado es true cuando no hay informacion personal para ese usuario
-    # Verificar si se ha creado una nueva instancia de InformacionPersonal
-    if creado:
-        request.method = 'POST'
-        request.POST = {
-            'fecha_nacimiento': None,
-            'edad': None,
-            'profesion': None,
-            'pais': None
-        }
-        return crear_informacion_personal(request)
-    else:
-        fecha_nacimiento = informacion_personal.fecha_nacimiento
-        edad = informacion_personal.edad
-        profesion = informacion_personal.profesion
-        pais = informacion_personal.pais
-        context={
-            'fecha_nacimiento': fecha_nacimiento,
-            'edad': edad,
-            'profesion': profesion,
-            'pais': pais
-        }
-    return render(request,'contacto/editar_informacion_personal.html',context)
-
-@login_required
-def crear_informacion_personal(request):
+    user = request.user
+    user_pi = InformacionPersonal.objects.get(usuario=user)
     if request.method == 'POST':
-        fecha_nacimiento = request.POST['fecha_nacimiento']
-        edad = request.POST['edad']
-        profesion = request.POST['profesion']
-        pais = request.POST['pais']
+        first_name = request.POST['input_first_name']
+        last_name = request.POST['input_last_name']
+        birthday = request.POST['input_birthday']
+        profesion = request.POST['input_profesion']
+        pais = request.POST['input_country']
 
-        # Crear nueva instancia de InformacionPersonal
-        informacion_personal = InformacionPersonal.objects.create(
-            usuario=request.user,
-            fecha_nacimiento=fecha_nacimiento,
-            edad=edad,
-            profesion=profesion,
-            pais=pais
-        )
-        informacion_personal.save()
+        user.first_name=first_name
+        user.last_name=last_name
+        user.save()
 
-    return redirect('edit_pi')
 
-    #return render(request, 'contacto/contacto.html')
+        user_pi.fecha_nacimiento = birthday
+        user_pi.profesion=profesion
+        user_pi.pais=pais
+        user_pi.save()
+        return redirect('contact')
+
+    context ={
+        "user":user,
+        "user_pi":user_pi
+    }
+    return render(request, 'contacto/editar_informacion_personal.html', context)
+
